d2526942ca9b0d6482be538dbb44865ee0ce6ac6
Lourdes.s <lou_santillan98@hotmail.com>
2026-01-16 15:11:42 -0300

    add texts ES/EN

--- src/texts.js
+++ src/texts.js
@@ -6,6 +6,24 @@ export const texts = {
             p2: "Desde hace años, he explorado el dibujo digital y la edición de imágenes, combinando mi amor por el arte con las herramientas tecnológicas. Durante la pandemia, se despertó en mí el interés por el desarrollo web.",
             p3: "Esta curiosidad me llevó a cursar una diplomatura en desarrollo Full Stack en la UTN. Aunque disfruto de los aspectos visuales y creativos del desarrollo, mi curiosidad y habilidades técnicas me han llevado a enfocarme en el backend.",
             contact: "Si quieres ponerte en contacto conmigo, escríbeme a"
+        },
+        technologies:{
+            title: "Tecnologías",
+            subtitle: "Herramientas"
+        },
+        projects:{
+            title: "Proyectos",
+            subtitleMsj: "Proyecto Mensajería",
+            p1Msj: "Aplicación web inspirada en WhatsApp Web, con login, registro, autenticación de email, chats entre usuarios y edición de perfil. Incluye diseño responsive y scroll infinito en los mensajes.",
+            p2Msj: "Usuario de prueba",
+            p3Msj: "Correo:",
+            p4Msj: "Contraseña:",
+            p5Msj: "Tecnologías utilizadas:",
+            p1Crm: "e-commerce que permite explorar un catálogo de productos, visualizar detalles, filtrar por categorías y gestionar un CRUD del carrito. CRUD de productos accesible solo por administradores.",
+            p2Crm: "Usuario de prueba (admin)",
+            p3Crm: "Correo:",
+            p4Crm: "Contraseña:",
+            p5Crm: "Tecnologías utilizadas:"
         }
     },
     en: {
@@ -15,6 +33,24 @@ export const texts = {
             p2: "For years, I explored digital drawing and image editing, combining my love for art with technology. During the pandemic, I became interested in web development.",
             p3: "This curiosity led me to complete a Full Stack Development program at UTN. While I enjoy visual and creative aspects, my technical skills have led me to focus on backend development.",
             contact: "If you want to get in touch with me, write me at"
+        },
+        technologies: {
+            title: "Technologies",
+            subtitle: "Tools"
+        },
+        projects: {
+            title: "Projects",
+            subtitleMsj:"Messaging App",
+            p1Msj: "Web application inspired by WhatsApp Web, featuring login, registration, email authentication, user-to-user chats, and profile editing. Includes a responsive design and infinite scroll for messages.",
+            p2Msj: "Test user",
+            p3Msj: "Email:",
+            p4Msj: "Password:",
+            p5Msj: "Technologies used:",
+            p1Crm: "E-commerce web application that allows users to browse a product catalog, view details, filter by categories, and manage a shopping cart CRUD. Product management CRUD is restricted to administrators only.",
+            p2Crm: "Test user (admin)",
+            p3Crm: "Email:",
+            p4Crm: "Password:",
+            p5Crm: "Technologies used:"
         }
     }
 }
